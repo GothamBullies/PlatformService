99d6e0b3a695e63f61dcce396869ffa98f878e97
Gotham Bullies <jimsell.morales@365connect.com>
2021-09-08 18:49:29 -0500

    update files

---
+++ Data/IPlatformRepo.cs
@@ -0,0 +1,16 @@
+﻿using PlatfromService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatfromService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform plat);
+    }
+}
---
+++ Data/PrepDb.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatfromService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatfromService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app) 
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope()) 
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());    
+            }            
+        }
+
+        private static void SeedData(AppDbContext context) 
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding data...");
+
+                context.Platforms.AddRange(
+                    new Platform() {Name="Dot Net", Publisher="Microsoft", Cost="Free" },
+                    new Platform() { Name = "SQL Server", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Kuberbates", Publisher = "Microsoft", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+            }
+            else {
+                 
+                Console.WriteLine("--> We already have Data");
+            }
+        }
+    }
+}
